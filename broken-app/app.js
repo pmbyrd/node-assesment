a3ac5bbca96012a60dfa3130b845db061d33d702
Perriesha Byrd <pmbyrd@gmail.com>
2023-06-17 15:14:19 -0700

    Created a middleware error handler

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,18 +1,23 @@
 const express = require('express');
-let axios = require('axios');
-var app = express();
+const axios = require('axios');
+const app = express();
+
 
 app.post('/', function(req, res, next) {
   try {
+    //handle error
     let results = req.body.developers.map(async d => {
       return await axios.get(`https://api.github.com/users/${d}`);
     });
     let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
-
+    console.log(out)
+    debugger;
     return res.send(JSON.stringify(out));
   } catch {
     next(err);
   }
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
--- broken-app/issues.md
+++ broken-app/issues.md
@@ -1,1 +1,8 @@
 # Broken App Issues
+
+## Variables
+
+Updated all variables to ES2016 standards.
+
+##### Errors
+Err is not defined.
